fe87bdf8eb190e22bc91c913e3675ade758a45d0
seba dorsch <sebastian.adorsch@gmail.com>
2021-07-24 00:17:18 -0300

    Ejercicios Python

--- filtro_caracteres.py
+++ filtro_caracteres.py
@@ -13,6 +13,13 @@ def solution(string, markers):
     return respuesta[:-1]
 
 
+def solution(string,markers):
+    parts = string.split('\n')
+    for s in markers:
+        parts = [v.split(s)[0].rstrip() for v in parts]
+    return '\n'.join(parts)
+
+
 if __name__ == '__main__':
     print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]),
                        "apples, pears\ngrapes\nbananas")
